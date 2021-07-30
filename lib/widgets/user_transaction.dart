e3974e3c57eb26561deb417259887d718a2fb601
antranvn <andevstudioth@gmail.com>
2021-07-30 09:25:34 +0700

    Add submit data

---
+++ lib/widgets/user_transaction.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_expense_planner/models/transaction.dart';
+import 'package:flutter_expense_planner/widgets/new_transaction.dart';
+import 'package:flutter_expense_planner/widgets/transaction_list.dart';
+
+class UserTransactions extends StatefulWidget {
+  const UserTransactions({Key? key}) : super(key: key);
+
+  @override
+  _UserTransactionsState createState() => _UserTransactionsState();
+}
+
+class _UserTransactionsState extends State<UserTransactions> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'Weekly Groceries',
+        amount: 16.53,
+        date: DateTime.now()),
+  ];
+
+  void _addNewTransaction(String title, double amount) {
+    final newTx = Transaction(
+      id: DateTime.now().toString(),
+      title: title,
+      amount: amount,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        NewTransaction(_addNewTransaction),
+        TransactionList(_userTransactions),
+      ],
+    );
+  }
+}
